13381cf4681cb405f2bb1af09449659ac04a452d
Markus Benning <me@w3r3wolf.de>
2013-09-02 09:46:18 +0200

    -mention proxy auth in POD docs

--- lib/LWP/Protocol/connect.pm
+++ lib/LWP/Protocol/connect.pm
@@ -49,6 +49,21 @@ LWP::Protocol::connect - Provides HTTP/CONNECT proxy support for LWP::UserAgent
 
   $ua->get('https://www.somesslsite.com');
 
+=head1 Proxy Authentication
+
+If you pass the authentication within the userinfo string of the proxy url
+a Basic authentication header will always be generated and sent to the proxy.
+
+  $ua->proxy('https', 'connect://user:pw@proxyhost.domain:3128');
+
+The more general way is to add the credentials of the proxy to the $ua object.
+
+  $ua->credentials("localhost:3128", "Squid proxy-caching web server", "user", "pw");
+
+In this case the first request to the proxy will fail with a "407 Proxy Auth. Required".
+This will cause LWP::UserAgent to choose the right LWP::Authen::<method> module to
+add the authentication and retry.
+
 =head1 DESCRIPTION
 
 The LWP::Protocol::connect module provides support for using https over
